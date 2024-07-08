ab2d11ad3f381c57b77ee08118128c0806a26a80
Комарова Ксения <xenia.komarr@gmail.com>
2024-07-08 19:01:30 +0300

    Остались шаблоны калькулятора

--- npogrpi/core/forms.py
+++ npogrpi/core/forms.py
@@ -1,9 +1,10 @@
 ﻿from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Ваше имя', max_length=100)
-    company = forms.CharField(label='Ваша компания', max_length=100)
-    telephone = forms.CharField(label='Ваш телефон', max_length=11)
-    email = forms.EmailField(label='Ваш Email', max_length=100)
-    message = forms.CharField(label='Ваше сообщение', widget=forms.Textarea)
+    name = forms.CharField(label=_('Ваше имя'), max_length=100)
+    company = forms.CharField(label=_('Ваша компания'), max_length=100)
+    telephone = forms.CharField(label=_('Ваш телефон'), max_length=11)
+    email = forms.EmailField(label=_('Ваш Email'), max_length=100)
+    message = forms.CharField(label=_('Ваше сообщение'), widget=forms.Textarea)
